ebf7e7a2ce714f2de7dddcabddc3a7e94740c253
Kira Kobzareva <kirakobzareva@gmail.com>
2022-08-07 16:33:53 +0200

    add reverse pipe

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): any {
+    return value.split('').reverse().join('');
   }
 
 }
